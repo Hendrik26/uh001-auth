5213505bcc8a9e84a5c26140624c1d509568495f
Hendrik <hendrik.herdler@gmail.com>
2019-03-19 16:53:14 +0100

    install modules and implement auth.service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBLepbxMmiV0SoH7stAVU8Vvxzligp7qRc",
+    authDomain: "uh001-65c6f.firebaseapp.com",
+    databaseURL: "https://uh001-65c6f.firebaseio.com",
+    projectId: "uh001-65c6f",
+    storageBucket: "uh001-65c6f.appspot.com",
+    messagingSenderId: "308265721577"
+  }
 };
 
 /*
